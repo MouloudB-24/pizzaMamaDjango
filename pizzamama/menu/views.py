5d8bd62abab80e0af3e5b3fe3a9323e784961331
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-10-15 05:56:33 +0200

    feat: version fonctionnelle

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Pizza
 
-# Create your views here.
+
+def index(request):
+    pizzas = Pizza.objects.all().order_by("prix")
+    return render(request, "menu/index.html", {"pizzas": pizzas})
